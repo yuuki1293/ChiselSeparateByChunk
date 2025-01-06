d349bb4bb679dbfa5f85919186f87a09ac7e028b
yuuki1293 <yuuki1293@outlook.jp>
2025-01-06 19:43:09 +0900

    chore: remove entry point

---
+++ forge/src/main/java/com/yuuki1293/chiselsbc/forge/ChiselSBCForge.java
@@ -0,0 +1,10 @@
+package com.yuuki1293.chiselsbc.forge;
+
+import com.yuuki1293.chiselsbc.ChiselSBC;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ChiselSBC.MOD_ID)
+public class ChiselSBCForge {
+    public ChiselSBCForge() {
+    }
+}

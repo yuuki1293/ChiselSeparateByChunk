2dc22eebd93efd53e3bd0f458ef67cbff974befe
yuuki1293 <yuuki1293@outlook.jp>
2025-01-29 13:24:40 +0900

    chore: rename example

---
+++ src/main/java/com/yuuki1293/chiselsbc/ChiselSBC.java
@@ -0,0 +1,7 @@
+package com.yuuki1293.chiselsbc;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, clientSideOnly = true)
+public class ChiselSBC {
+}

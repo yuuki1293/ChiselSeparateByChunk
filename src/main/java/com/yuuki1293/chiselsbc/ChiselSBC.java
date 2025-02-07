35dfd602f49c40c321e14dd170f536e6bd93132a
yuuki1293 <yuuki1293@outlook.jp>
2025-02-07 22:25:43 +0900

    chore: delete chisel reborn

--- src/main/java/com/yuuki1293/chiselsbc/ChiselSBC.java
+++ src/main/java/com/yuuki1293/chiselsbc/ChiselSBC.java
@@ -1,7 +1,5 @@
 package com.yuuki1293.chiselsbc;
 
-import earth.terrarium.athena.api.client.utils.AthenaUtils;
-import net.minecraft.core.Direction;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(ChiselSBC.MOD_ID)
@@ -9,49 +7,5 @@ public class ChiselSBC {
     public static final String MOD_ID = "chiselsbc";
 
     public ChiselSBC() {
-    }
-
-    /**
-     * Get {@link Direction} from {@link AthenaUtils.UrMom}.
-     */
-    public static Direction fromUrMom(Direction facing, AthenaUtils.UrMom urMom) {
-        return switch (facing) {
-            case UP -> switch (urMom) {
-                case UP -> Direction.NORTH;
-                case DOWN -> Direction.SOUTH;
-                case LEFT -> Direction.WEST;
-                case RIGHT -> Direction.EAST;
-            };
-            case DOWN -> switch (urMom) {
-                case UP -> Direction.SOUTH;
-                case DOWN -> Direction.NORTH;
-                case LEFT -> Direction.WEST;
-                case RIGHT -> Direction.EAST;
-            };
-            case NORTH -> switch (urMom) {
-                case UP -> Direction.UP;
-                case DOWN -> Direction.DOWN;
-                case LEFT -> Direction.EAST;
-                case RIGHT -> Direction.WEST;
-            };
-            case SOUTH -> switch (urMom) {
-                case UP -> Direction.UP;
-                case DOWN -> Direction.DOWN;
-                case LEFT -> Direction.WEST;
-                case RIGHT -> Direction.EAST;
-            };
-            case WEST -> switch (urMom) {
-                case UP -> Direction.UP;
-                case DOWN -> Direction.DOWN;
-                case LEFT -> Direction.NORTH;
-                case RIGHT -> Direction.SOUTH;
-            };
-            case EAST -> switch (urMom) {
-                case UP -> Direction.UP;
-                case DOWN -> Direction.DOWN;
-                case LEFT -> Direction.SOUTH;
-                case RIGHT -> Direction.NORTH;
-            };
-        };
     }
 }
